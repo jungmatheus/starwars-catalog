29d76f257a036b32a04f509b7c1901e3600258a8
splarsxd <splarsiz34@yahoo.com>
2020-11-01 16:24:44 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -21,6 +21,24 @@ async function request() {
     let data = await response.json()
     console.log(data.name)
     console.log(data.height)
+    console.log(data.mass)
+    console.log(data.hair_color)
+    console.log(data.skin_color)
+    console.log(data.eye_color)
+    console.log(data.birth_year)
+    console.log(data.gender)
+
+    response = await fetch(data.homeworld)
+    data = await response.json()
+
+    console.log(data.name)
+    console.log(data.rotation_period)
+    console.log(data.orbital_period)
+    console.log(data.diameter)
+    console.log(data.climate)
+    console.log(data.gravity)
+    console.log(data.terrain)
+    console.log(data.population)
 	//document.getElementByClassName("name")[0].innerHTML = name
 }
 
