81f494d464efae9ac1c5d1e31d4e43b27eb30092
Matheus Jung <69332612+jungmatheus@users.noreply.github.com>
2020-11-01 16:24:32 +0100

    request json file

--- script/script.js
+++ script/script.js
@@ -1,9 +1,18 @@
 let li = document.querySelectorAll("li");
 
+async function request() {
 
+
+    const getRequest = await fetch("https://swapi.dev/api/people/");
+    const data = await getRequest.json();
+
+    console.log(data);
+}
+
+request();
 
 for(let i = 0; i < li.length; i++) {
-    
+
     li[i].addEventListener("click", function() {
 
         li[i].classList.add("li-clicked");
